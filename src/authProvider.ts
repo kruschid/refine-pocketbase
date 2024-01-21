41c087e1f216ae18d7e6473af953615fcf2826e6
Denis Kruschinski <4989565+kruschid@users.noreply.github.com>
2024-01-21 18:49:46 +0100

    fixes auth options

--- src/authProvider.ts
+++ src/authProvider.ts
@@ -10,86 +10,96 @@ export interface LoginWithPassword {
   remember: boolean;
 }
 
-type LoginOptions = LoginWithProvider | LoginWithPassword;
+export type LoginOptions = LoginWithProvider | LoginWithPassword;
+
+export interface AuthOptions {
+  loginRedirectTo?: string;
+  loginErrorRedirectTo?: string;
+  logoutRedirectTo?: string;
+  unauthenticatedRedirectTo?: string;
+}
+
+const defaultOptions: AuthOptions = {
+  loginRedirectTo: "/",
+  loginErrorRedirectTo: undefined,
+  logoutRedirectTo: "/login",
+  unauthenticatedRedirectTo: "/login",
+};
 
 const isLoginWithProvider = (x: any): x is LoginWithProvider =>
   typeof x.providerName === "string";
 
 export const authProvider = (
   pb: PocketBase,
-  options = {
-    loginRedirectTo: "/",
-    loginErrorRedirectTo: undefined,
-    logoutRedirectTo: "/login",
-    checkRedirectTo: "/login",
-  }
-): AuthBindings => ({
-  login: async (loginOptions: LoginOptions) => {
-    if (isLoginWithProvider(loginOptions)) {
-      await pb
-        .collection("users")
-        .authWithOAuth2({ provider: loginOptions.providerName })
-        .catch(() => null);
+  authOptions?: AuthOptions
+): AuthBindings => {
+  const options = { ...defaultOptions, ...authOptions };
+  return {
+    login: async (loginOptions: LoginOptions) => {
+      if (isLoginWithProvider(loginOptions)) {
+        await pb
+          .collection("users")
+          .authWithOAuth2({ provider: loginOptions.providerName });
+        if (pb.authStore.isValid) {
+          return {
+            success: true,
+            redirectTo: options.loginRedirectTo,
+          };
+        }
+      } else {
+        await pb
+          .collection("users")
+          .authWithPassword(loginOptions.email, loginOptions.password);
+        if (pb.authStore.isValid) {
+          return {
+            success: true,
+            redirectTo: options.loginRedirectTo,
+          };
+        }
+      }
+
+      return {
+        success: false,
+        error: {
+          name: "Login error",
+          message: "Invalid credentials",
+        },
+        redirectTo: options.loginErrorRedirectTo,
+      };
+    },
+    logout: async () => {
+      pb.authStore.clear();
+      return {
+        success: true,
+        redirectTo: options.logoutRedirectTo,
+      };
+    },
+    check: async () => {
       if (pb.authStore.isValid) {
         return {
-          success: true,
-          redirectTo: options.loginRedirectTo,
+          authenticated: true,
         };
       }
-    } else {
-      await pb
-        .collection("users")
-        .authWithPassword(loginOptions.email, loginOptions.password)
-        .catch(() => null);
-      if (pb.authStore.isValid) {
+
+      return {
+        authenticated: false,
+        redirectTo: options.unauthenticatedRedirectTo,
+      };
+    },
+    getPermissions: async () => null,
+    getIdentity: async () => {
+      if (pb.authStore.isValid && pb.authStore.model) {
+        const { id, name, avatar } = pb.authStore.model;
         return {
-          success: true,
-          redirectTo: options.loginRedirectTo,
+          id,
+          name,
+          avatar,
         };
       }
-    }
-
-    return {
-      success: false,
-      error: {
-        name: "Login error",
-        message: "Invalid credentials",
-      },
-      redirectTo: options.loginErrorRedirectTo,
-    };
-  },
-  logout: async () => {
-    pb.authStore.clear();
-    return {
-      success: true,
-      redirectTo: options.logoutRedirectTo,
-    };
-  },
-  check: async () => {
-    if (pb.authStore.isValid) {
-      return {
-        authenticated: true,
-      };
-    }
-
-    return {
-      authenticated: false,
-      redirectTo: options.checkRedirectTo,
-    };
-  },
-  getPermissions: async () => null,
-  getIdentity: async () => {
-    if (pb.authStore.isValid && pb.authStore.model) {
-      const { id, name, avatar } = pb.authStore.model;
-      return {
-        id,
-        name,
-        avatar,
-      };
-    }
-    return null;
-  },
-  onError: async (error) => {
-    return { error };
-  },
-});
+      return null;
+    },
+    onError: async (error) => {
+      return { error };
+    },
+  };
+};
