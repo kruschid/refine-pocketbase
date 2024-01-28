0e3451ba451f14dbdd61db88ec87d25d3f16b058
Denis Kruschinski <4989565+kruschid@users.noreply.github.com>
2024-01-28 17:48:40 +0100

    adds register, forgotPassword, updatePassword

--- src/authProvider.ts
+++ src/authProvider.ts
@@ -13,17 +13,24 @@ export interface LoginWithPassword {
 export type LoginOptions = LoginWithProvider | LoginWithPassword;
 
 export interface AuthOptions {
+  collection: string;
+  requestVerification: boolean;
+  registerRedirectTo?: string;
+  registerErrorRedirectTo?: string;
+  forgotPasswordRedirectTo?: string;
+  forgotPasswordErrorRedirectTo?: string;
+  updatePasswordRedirectTo?: string;
+  updatePasswordErrorRedirectTo?: string;
   loginRedirectTo?: string;
   loginErrorRedirectTo?: string;
   logoutRedirectTo?: string;
+  authenticatedRedirectTo?: string;
   unauthenticatedRedirectTo?: string;
 }
 
 const defaultOptions: AuthOptions = {
-  loginRedirectTo: "/",
-  loginErrorRedirectTo: undefined,
-  logoutRedirectTo: "/login",
-  unauthenticatedRedirectTo: "/login",
+  collection: "users",
+  requestVerification: false,
 };
 
 const isLoginWithProvider = (x: any): x is LoginWithProvider =>
@@ -31,14 +38,69 @@ const isLoginWithProvider = (x: any): x is LoginWithProvider =>
 
 export const authProvider = (
   pb: PocketBase,
-  authOptions?: AuthOptions
+  authOptions?: Partial<AuthOptions>
 ): AuthBindings => {
-  const options = { ...defaultOptions, ...authOptions };
+  const options: AuthOptions = { ...defaultOptions, ...authOptions };
+
   return {
+    register: async ({ email, password }) => {
+      try {
+        await pb.collection(options.collection).create({
+          email,
+          password,
+          passwordConfirm: password,
+        });
+
+        if (options.requestVerification) {
+          await pb.collection(options.collection).requestVerification(email);
+        }
+        return {
+          success: true,
+          redirectTo: options.registerRedirectTo,
+        };
+      } catch (err) {
+        return {
+          success: false,
+          redirectTo: options.registerErrorRedirectTo,
+        };
+      }
+    },
+    forgotPassword: async ({ email }) => {
+      try {
+        await pb.collection(options.collection).requestPasswordReset(email);
+
+        return {
+          success: true,
+          redirectTo: options.forgotPasswordRedirectTo,
+        };
+      } catch (err) {
+        return {
+          success: false,
+          redirectTo: options.forgotPasswordErrorRedirectTo,
+        };
+      }
+    },
+    updatePassword: async ({ token, password, confirmPassword }) => {
+      try {
+        await pb
+          .collection(options.collection)
+          .confirmPasswordReset(token, password, confirmPassword);
+
+        return {
+          success: true,
+          redirectTo: options.updatePasswordRedirectTo,
+        };
+      } catch (err) {
+        return {
+          success: false,
+          redirectTo: options.updatePasswordErrorRedirectTo,
+        };
+      }
+    },
     login: async (loginOptions: LoginOptions) => {
       if (isLoginWithProvider(loginOptions)) {
         await pb
-          .collection("users")
+          .collection(options.collection)
           .authWithOAuth2({ provider: loginOptions.providerName });
         if (pb.authStore.isValid) {
           return {
@@ -48,7 +110,7 @@ export const authProvider = (
         }
       } else {
         await pb
-          .collection("users")
+          .collection(options.collection)
           .authWithPassword(loginOptions.email, loginOptions.password);
         if (pb.authStore.isValid) {
           return {
@@ -78,6 +140,7 @@ export const authProvider = (
       if (pb.authStore.isValid) {
         return {
           authenticated: true,
+          redirectTo: options.authenticatedRedirectTo,
         };
       }
 
