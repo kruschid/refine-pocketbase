b1f640a06d4940128c5f3646968d7076e73dad64
Denis Kruschinski <4989565+kruschid@users.noreply.github.com>
2024-02-01 21:28:16 +0100

    adds inbucket

--- pocketbase/volumes/pb_migrations/1706643677_create_admin.js
+++ pocketbase/volumes/pb_migrations/1706643677_create_admin.js
@@ -1,16 +1,24 @@
 /// <reference path="../pb_data/types.d.ts" />
 migrate(
   (db) => {
+    const dao = new Dao(db);
+
+    const settings = dao.findSettings();
+    settings.smtp.enabled = true;
+    settings.smtp.host = "inbucket";
+    settings.smtp.port = 2500;
+    settings.meta.resetPasswordTemplate.body = "{TOKEN}";
+    dao.saveSettings(settings);
+
     const admin = new Admin();
     admin.email = "test@example.com";
     admin.setPassword(1234567890);
-
-    new Dao(db).saveAdmin(admin);
+    dao.saveAdmin(admin);
   },
   (db) => {
     const dao = new Dao(db);
     const admin = dao.findAdminByEmail("test@example.com");
 
-    return dao.deleteAdmin(admin);
+    dao.deleteAdmin(admin);
   }
 );
