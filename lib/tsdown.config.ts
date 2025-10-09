9f0c8f9ccb21c8b9df83baa483c521253c28e5de
Denis Kruschinski <4989565+kruschid@users.noreply.github.com>
2025-10-09 21:59:10 +0200

    pnpm tsdown migration

---
+++ lib/tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsdown'
+import packagejson from './package.json' with { type: 'json' }
+
+export default defineConfig({
+  entry: ['./src/index.ts'],
+  dts: true,  
+  platform: "browser",
+  treeshake: true,
+  external: [
+    ...Object.keys(packagejson.devDependencies),
+  ],
+});
