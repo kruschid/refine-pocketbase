352e9a6c93bf55e159c32a0b1d69ea93148aa756
Denis Kruschinski <4989565+kruschid@users.noreply.github.com>
2025-10-10 14:28:02 +0200

    demo playwright uses pnpm script

--- demo/playwright.config.ts
+++ demo/playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "pnpm dev",
     url: baseURL,
     reuseExistingServer: !process.env.CI,
   },
